f3b58d8e183fc974e29d72c020544ecaa16527cc
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-19 09:03:42 +0700

    #8 - Add WebSecurityConfig

---
+++ src/main/java/com/hendisantika/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-reservation-system
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/03/22
+ * Time: 09.03
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private final String[] SECURITY_IGNORED_ENDPOINTS = {
+            "/h2-console/**", "/h2/**", "/webjars/**"
+    };
+
+    public WebSecurityConfig(UserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+}
