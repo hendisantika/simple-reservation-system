30f122d3a772dc9dca7cc764ce3fdd386b70b84d
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-19 09:20:30 +0700

    #35 - Add reservations in PagesController

--- src/main/java/com/hendisantika/controller/PagesController.java
+++ src/main/java/com/hendisantika/controller/PagesController.java
@@ -1,9 +1,16 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Reservation;
+import com.hendisantika.model.User;
 import com.hendisantika.service.ReservationService;
 import com.hendisantika.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,6 +35,26 @@ public class PagesController {
 
     @GetMapping("/")
     public String index() {
+        return "index";
+    }
+
+    @GetMapping("/reservations")
+    public String reservations(Model model, HttpSession session) {
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String name = principal.getUsername();
+        User user = userService.getUserByUsername(name);
+
+        // This should always be the case
+        if (user != null) {
+            session.setAttribute("user", user);
+
+            // Empty reservation object in case the user creates a new reservation
+            Reservation reservation = new Reservation();
+            model.addAttribute("reservation", reservation);
+
+            return "reservations";
+        }
+
         return "index";
     }
 }
