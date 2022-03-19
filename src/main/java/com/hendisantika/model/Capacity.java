731cbf18d0ace7f80610cd4091a8025f6df99bfe
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-19 09:05:40 +0700

    #11 - Add Capacity Model

---
+++ src/main/java/com/hendisantika/model/Capacity.java
@@ -0,0 +1,52 @@
+package com.hendisantika.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-reservation-system
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/03/22
+ * Time: 09.05
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Capacity {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @SequenceGenerator(
+            name = "primary_sequence",
+            sequenceName = "primary_sequence",
+            allocationSize = 1,
+            initialValue = 10000
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "primary_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @Enumerated(EnumType.STRING)
+    private AmenityType amenityType;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    public Capacity(AmenityType amenityType, int capacity) {
+        this.amenityType = amenityType;
+        this.capacity = capacity;
+    }
+}
