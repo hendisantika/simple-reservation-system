eba3991d0fc6ee4f10f2c7f7ad995b23ffe14d4c
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-19 09:04:05 +0700

    #9 - Add configure in WebSecurityConfig

--- src/main/java/com/hendisantika/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/config/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.hendisantika.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,4 +34,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(SECURITY_IGNORED_ENDPOINTS);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .logoutSuccessUrl("/");
+    }
+
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
 }
