a8c74217c6270c3fe3c9ac75d59bfbaeb6b16857
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-19 09:09:01 +0700

    #17 - Add UserService

---
+++ src/main/java/com/hendisantika/service/UserService.java
@@ -0,0 +1,25 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : simple-reservation-system
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/03/22
+ * Time: 09.08
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
